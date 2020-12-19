10d0c44394f4958d779ed3357dcaa3ba00a0d80c
Ryan Chris Taja-on <rybeb2214@gmail.com>
2020-12-19 19:43:40 +0800

    create sample users in mongodb

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -15,7 +15,7 @@ function App() {
         <header className='row'>
           <div>
             <Link className='brand' to='/'>
-              amazona
+              lodi
             </Link>
           </div>
           <div>
